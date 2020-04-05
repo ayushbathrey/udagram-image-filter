a8549663db7fbd768dc81a51d91bc2b190ef0cfb
ayushbathrey <ayushbathrey3@gmail.com>
2020-04-05 23:11:49 +0530

    refactor: Refactoring endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Response, Request } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -30,7 +30,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  app.get( "/filteredimage", async ( req, res ) => {
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
     var input_url : string = req.query.image_url;
 
     if ( !input_url ) {
@@ -47,7 +47,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   });
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
